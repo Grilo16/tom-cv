c2acecdb6ecfd4778e6363ba22fbd45dc7c5a304
Grilo16 <98457056+Grilo16@users.noreply.github.com>
2022-11-26 12:52:51 +0000

    just in case tests dont work

--- src/components/LayoutComponents/OrbComponent.jsx
+++ src/components/LayoutComponents/OrbComponent.jsx
@@ -1,34 +1,87 @@
-import styled from "styled-components";
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styled, {Keyframes} from "styled-components";
+import pdfIcon from "../../static/icons/pdfIcon.png"
+import C from "../../static/ProgramingLanguagesIcons/C.png"
+import Css from "../../static/ProgramingLanguagesIcons/Css.png"
+import Flask from "../../static/ProgramingLanguagesIcons/Flask.png"
+import HTML from "../../static/ProgramingLanguagesIcons/HTML.png"
+import Java from "../../static/ProgramingLanguagesIcons/Java.png"
+import Javascript from "../../static/ProgramingLanguagesIcons/Javascript.png"
+import MongoDB from "../../static/ProgramingLanguagesIcons/MongoDB.png"
+import Postgres from "../../static/ProgramingLanguagesIcons/Postgres.png"
+import Python from "../../static/ProgramingLanguagesIcons/Python.png"
+import React from "../../static/ProgramingLanguagesIcons/React.png"
+import Spring from "../../static/ProgramingLanguagesIcons/Spring.png"
+import { keyframes } from "styled-components";
 const OrbComponent = () => {
+
+
+  
+  const [selectedOrb, setSelectedOrb] = useState("")
+  
+  const stackIcons = [C, Css, Flask, HTML, Java, Javascript, MongoDB, Postgres, Python, React, Spring, C, Css, Flask, HTML, Java, Javascript, MongoDB, Postgres, Python, React, Spring].map(icon => <img height={"125vw"} src={icon}/>) 
+
+
+
+
   const height = 23;
   const width = height;
+
+const handleSelectOrb = () =>{
+  setSelectedOrb()
+}
 
   return (
     <div style={{ overflow: "hidden" }}>
       <OrbDiv>
 
-        <FirstNameH1 height={height}>Tom</FirstNameH1>
-        <JobTitleP>Software developer</JobTitleP>
-
         <OrbElement
-          gridRow={"6/16"}
+          gridRow={"4/8"}
           gridColumn={"8/16"}
           height={height}
           width={width}
           noHover={true}
           >
+            <div style={{display: "flex", gap: "2vw", justifyContent: "space-evenly", marginRight: "15vw"}}>
+      <div>
+
+        <FirstNameH1 height={height}>Tom</FirstNameH1>
+        <JobTitleP>Software developer</JobTitleP>
+      </div>
           <SurnameH1> Britton </SurnameH1>{" "}
+            </div>
         </OrbElement>
 
+{selectedOrb === "1" ? 
         <OrbElement
-          gridRow={"6/16"}
-          gridColumn={"17/20"}
-          height={height / 2}
-          width={width / 2}
+        gridRow={"4/5"}
+        gridColumn={"17/20"}
+        height={height/1.5}
+        width={width+20}
+        borderRadius={"20"}
+        onClick={()=>setSelectedOrb("0")}
+        scale={"1.1"}
+        noHover={true}
+        marginLeft={2}
+        style={{overflow: "hidden"}}
+        >
+          <IconHolderDiv style={{display: "flex", gap:"1vw"}}>
+
+          {stackIcons}
+          </IconHolderDiv>
+        </OrbElement>
+          :
+        <OrbElement
+        gridRow={"4/5"}
+        gridColumn={"17/20"}
+        height={height / 2}
+        width={width / 2}
+        onClick={()=>setSelectedOrb("1")}
         >
           {" "}
         </OrbElement>
+        }
 
         <OrbElement
           gridRow={"2/4"}
@@ -40,12 +93,15 @@ const OrbComponent = () => {
         </OrbElement>
 
         <OrbElement
-          gridRow={"15/16"}
+          gridRow={"7"}
           gridColumn={"7"}
           height={height / 8}
           width={width / 8}
         >
-          {" "}
+          <Link to={"pdf"} style={{textDecoration: "none", color: "#c2c4d1", textAlign: "center"}}>
+          <OrbTextP>cv</OrbTextP>
+          <OrbTextP backgroundColor={"#585f69"}>pdf</OrbTextP>
+          </Link>
         </OrbElement>
 
         <OrbElement
@@ -58,7 +114,7 @@ const OrbComponent = () => {
         </OrbElement>
 
         <OrbElement
-          gridRow={"14/16"}
+          gridRow={"6/8"}
           gridColumn={"17"}
           height={height / 4}
           width={width / 4}
@@ -80,7 +136,7 @@ const OrbComponent = () => {
           gridColumn={"1"}
           width={height / 8}
           height={40}
-          marginLeft={"-19"}
+          marginLeft={"-30"}
           marginTop={"-5.5"}
         >
           {" "}
@@ -91,7 +147,7 @@ const OrbComponent = () => {
           gridColumn={"1"}
           width={height / 8}
           height={45}
-          marginLeft={"-16"}
+          marginLeft={"-27"}
           marginTop={"-6"}
         >
           {" "}
@@ -131,23 +187,16 @@ const JobTitleP = styled.p`
   font-size: 1.5vw;
   font-weight: 600;
   color: #515369;
-  height: max-content;
-  grid-row: 13/14;
-  grid-column: 1/8;
   margin: 0;
   margin-top: -1vw;
-  margin-bottom: 1vw;
 `;
 
 const FirstNameH1 = styled.h1`
+  position: relative;
   font-size: 6.9vw;
   font-weight: 600;
   color: #515369;
-  height: max-content;
-  grid-row: 12/12;
-  grid-column: 1/8;
   margin: 0;
-  margin-right: 1vw;
 `;
 
 const SurnameH1 = styled.h1`
@@ -172,17 +221,20 @@ const OrbElement = styled.div`
   background-color: #515369;
   grid-row: ${(props) => (props.gridRow ? props.gridRow : null)};
   grid-column: ${(props) => (props.gridColumn ? props.gridColumn : null)};
-  border-radius: 50%;
+  border-radius: ${(props) => (props.borderRadius ? props.borderRadius + "vw" : "50%")};
+  scale: ${(props) => (props.scale ? props.scale  : null)};
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  margin-left: ${(props)=>props.marginLeft ? props.marginLeft + "vw" : null};
+
   &:hover {
     ${
       (props) =>
         props.noHover
-          ? null
-          : "transition: 0.5s; border-radius: 10px; transform: rotate(180deg); scale: 2; margin: 1vw; position: relative;"
+          ? "transition: 0.5s;"
+          : "transition: 0.5s; scale: 1.1; "
       // scale:  ${(props)=>props.noHover ? null : "1.1"};
       //   height: ${(props) => (props.height ? props.height + 15 + "vw" : "80vw")};
       //   width: ${(props) => (props.width ? props.width + 15 + "vw" : "40vw")};
@@ -210,4 +262,27 @@ const DiagonalBarElement = styled.div`
   transform: rotate(45deg);
 `;
 
+
+const IconScrollAnimation = keyframes`
+0% {margin-left: 0vw}
+100% {margin-left: 196.3vw}
+`
+const IconHolderDiv = styled.div`
+display: flex;
+gap: 2vw;
+margin-right: 160vw;
+animation: ${IconScrollAnimation} 20s linear infinite;
+&:hover {
+  animation-play-state: paused;  
+}
+
+`
+
+const OrbTextP = styled.p`
+color: #c2c4d1;
+margin: 0;
+background-color: ${(props) => props.backgroundColor ? props.backgroundColor : null};
+border-radius: 1vw;
+width:  ${(props) => props.backgroundColor ? "2vw" : null};
+`
 export default OrbComponent;
